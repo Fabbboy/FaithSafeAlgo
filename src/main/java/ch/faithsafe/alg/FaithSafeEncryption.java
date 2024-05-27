db8f629a62c7d39be1d0ade8d87d4275cd10044d
Lon60 <gitlon@proton.me>
2024-05-27 13:55:52 +0200

    hardcode SEED

--- src/main/java/ch/faithsafe/alg/FaithSafeEncryption.java
+++ src/main/java/ch/faithsafe/alg/FaithSafeEncryption.java
@@ -8,11 +8,23 @@ import javax.crypto.Cipher;
 public class FaithSafeEncryption {
     private KeyPair keyPair;
 
-    // Constructor that initializes the KeyPair
-    public FaithSafeEncryption(byte[] initialSeed) {
+    // Hardcoded seed
+    private static final byte[] HARDCODED_SEED = new byte[] {
+        (byte)0x01, (byte)0x23, (byte)0x45, (byte)0x67,
+        (byte)0x89, (byte)0xab, (byte)0xcd, (byte)0xef,
+        (byte)0x12, (byte)0x34, (byte)0x56, (byte)0x78,
+        (byte)0x9a, (byte)0xbc, (byte)0xde, (byte)0xf0,
+        (byte)0x21, (byte)0x43, (byte)0x65, (byte)0x87,
+        (byte)0xa9, (byte)0xcb, (byte)0xed, (byte)0x0f,
+        (byte)0x31, (byte)0x53, (byte)0x75, (byte)0x97,
+        (byte)0xb9, (byte)0xdb, (byte)0xfd, (byte)0x10
+    };
+
+    // Constructor that initializes the KeyPair with the hardcoded seed
+    public FaithSafeEncryption() {
         try {
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-            SecureRandom seed = new SecureRandom(initialSeed);
+            SecureRandom seed = new SecureRandom(HARDCODED_SEED);
             keyGen.initialize(2048, seed);
             this.keyPair = keyGen.generateKeyPair();
         } catch (Exception e) {
